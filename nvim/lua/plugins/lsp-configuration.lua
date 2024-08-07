95f4d7cc4466bdd002a7b790ec196e4838a7a781
Itallo Sá Vieira <itllsv@Itallos-MacBook-Pro.local>
2024-08-07 17:31:11 -0300

    initial commit

---
+++ nvim/lua/plugins/lsp-configuration.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_lsp",
+          "tsserver"
+        }
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+      lspconfig.tsserver.setup({})
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, {})
+    end,
+  }
+}
